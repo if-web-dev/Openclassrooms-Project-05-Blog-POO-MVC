0e5086ab5b2814248e2ab5655e257cd385597499
ishake fouhal <i.fouhal@hotmail.com>
2022-12-01 10:22:48 +0100

    logo edited

--- app/Views/Common/header.view.php
+++ app/Views/Common/header.view.php
@@ -1,7 +1,11 @@
  <!-- Responsive navbar-->
  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
      <div class="container px-5">
-         <a class="navbar-brand" href="#!">IF-WEB-DEV</a>
+        
+         <a class="navbar-brand" href="home">
+            <img src="assets/img/if-logo.png" height="60" alt="..." />
+            IF-WEB-DEV
+        </a>
          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
          <div class="collapse navbar-collapse" id="navbarSupportedContent">
              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
--- app/Views/home.view.php
+++ app/Views/home.view.php
@@ -14,7 +14,9 @@
 
     <!-- Heading Row-->
     <div class="row gx-4 gx-lg-5 align-items-center my-5">
-        <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0 shadow" src="assets/img/img-home.png" alt="..." /></div>
+        <div class="col-lg-7 text-center">
+            <img class="rounded mb-4 mb-lg-0" src="assets/img/home-page-logo-last.png" height="265" alt="..." />
+        </div>
         <div class="col-lg-5">
             <h1 class="font-weight-light">Home Page</h1>
             <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components.</p>
